ac18bcd0bc4a3b3ecf13a028e3b24bc2b25d6785
Sylvain CECCHETTO <cecchetto.sylvain@me.com>
2021-09-12 10:49:03 +0200

    Update upstream version to 4.3.1979

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,5 +9,5 @@ pkg_dependencies_x86="libavahi-client3 libavahi-common3 libc6 libdbus-1-3 libssl
 pkg_dependencies_rb="libavahi-client3 libavahi-common3 libc6 libdbus-1-3 libssl1.1 liburiparser1 zlib1g dvb-apps bzip2"
 
 # deb package URLs
-tvheadend_deb_x86_64="https://github.com/YunoHost-Apps/tvheadend_ynh/raw/debs/4.2.8-36/tvheadend_4.2.8-36%7Eg5bdcfd8ac%7Estretch_amd64.deb"
-tvheadend_deb_arm="https://github.com/YunoHost-Apps/tvheadend_ynh/raw/debs/4.2.8-36/tvheadend_4.2.8-36%7Eg5bdcfd8ac%7Eraspbianstretch_armhf.deb"
+tvheadend_deb_x86_64="https://github.com/YunoHost-Apps/tvheadend_ynh/raw/debs/4.3.1979/tvheadend_4.3-1979%7Eg8fc2dfa7e%7Estretch_amd64.deb"
+tvheadend_deb_arm="https://github.com/YunoHost-Apps/tvheadend_ynh/raw/debs/4.3.1979/tvheadend_4.3-1979%7Eg8fc2dfa7e%7Eraspbianstretch_armhf.deb"
