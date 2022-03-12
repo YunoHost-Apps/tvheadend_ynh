93faf9bded80dab0df80f7cafe574f20a2c21571
Sylvain CECCHETTO <cecchetto.sylvain@me.com>
2022-03-12 13:14:13 +0100

    Bump upstream to 4.3.2009

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -9,8 +9,8 @@ pkg_dependencies_x86="libavahi-client3 libavahi-common3 libc6 libdbus-1-3 libdvb
 pkg_dependencies_rb="libavahi-client3 libavahi-common3 libc6 libdbus-1-3 libdvbcsa1 libpcre2-8-0 libssl1.1 liburiparser1 zlib1g bzip2"
 
 # deb package URLs
-tvheadend_deb_x86="https://github.com/YunoHost-Apps/tvheadend_ynh/raw/debs/4.3.1979/tvheadend_4.3-1979%7Eg8fc2dfa7e%7Estretch_amd64.deb"
-tvheadend_deb_arm="https://github.com/YunoHost-Apps/tvheadend_ynh/raw/debs/4.3.1979/tvheadend_4.3-1979%7Eg8fc2dfa7e%7Eraspbianstretch_armhf.deb"
+tvheadend_deb_x86="https://github.com/YunoHost-Apps/tvheadend_ynh/raw/debs/4.3.2009/tvheadend_4.3-2009%7Eg1295dd2be%7Estretch_amd64.deb"
+tvheadend_deb_arm="https://github.com/YunoHost-Apps/tvheadend_ynh/raw/debs/4.3.2009/tvheadend_4.3-2009%7Eg1295dd2be%7Eraspbianstretch_armhf.deb"
 
 #=================================================
 # PERSONAL HELPERS
