f737e3603c90ce8720d8e9d764bc37f58eac377d
Félix Piédallu <felix@piedallu.me>
2025-01-09 21:55:26 +0100

    Use the repository instead of a deb package

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -1,24 +1,17 @@
 #!/bin/bash
 
 _install_tvheadend_package() {
-    # Download the package
-    if [ ! -f "$install_dir/tvheadend.deb" ]; then
-        ynh_setup_source --dest_dir="$install_dir" --source_id="main_$YNH_DEBIAN_VERSION"
-    fi
-
     # Pre-seed debconf database with answers of the interactive dpkg
     echo tvheadend tvheadend/admin_password password "$password" | debconf-set-selections
     echo tvheadend tvheadend/admin_username string "$superuser" | debconf-set-selections
     echo tvheadend tvheadend/last_notes note | debconf-set-selections
 
-    # Install the package
-    ynh_package_install \
-        "$install_dir/tvheadend.deb"
+    ynh_install_extra_app_dependencies \
+        --repo="deb https://dl.cloudsmith.io/public/tvheadend/tvheadend/deb/debian $YNH_DEBIAN_VERSION main" \
+        --key="https://dl.cloudsmith.io/public/tvheadend/tvheadend/gpg.C6CC06BD69B430C6.key" \
+        --package="tvheadend" \
 
     chown -R "hts:video" "$data_dir"
-
-    # Mark packages as dependencies, to allow automatic removal
-    apt-mark auto tvheadend
 
     # Prevent tvheadend being upgraded through apt…
     apt-mark hold tvheadend
